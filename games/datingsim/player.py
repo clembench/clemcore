3a315bb65707ce2f7b9512862e2fd3433508ba4b
Jerycho Pasniczek <pasniczek@uni-potsdam.de>
2024-06-25 18:53:45 +0200

    Adding players for informative meaning

--- games/datingsim/player.py
+++ games/datingsim/player.py
@@ -1,14 +1,40 @@
-#here implement class player
+# here implement class player
 from typing import List, Tuple, Dict, Any
 
 from clemgame.clemgame import Player
 
 
 class PC(Player):
-    def __init__(self, model_name: str, player:str):
+    def __init__(self, model_name: str, player: str):
         super().__init__(model_name)
         self.player: str = player
         self.history: List = []
-        #make it a dict maybe? and include turns
+        # make it a dict maybe? and include turns
 
     def __call__(self, messages: List[Dict], turn_idx) -> Tuple[Any, Any, str]:
+        # all the important stuff goes here
+        return None
+
+
+class NPC(Player):
+    def __init__(self, model_name: str, player: str):
+        super().__init__(model_name)
+        self.player: str = player
+        self.history: List = []
+        # make it a dict maybe? and include turns
+
+    def __call__(self, messages: List[Dict], turn_idx) -> Tuple[Any, Any, str]:
+        # all the important stuff goes here
+        return None
+
+
+class Assistant(Player):
+    def __init__(self, model_name: str, player: str):
+        super().__init__(model_name)
+        self.player: str = player
+        self.history: List = []
+        # make it a dict maybe? and include turns
+
+    def __call__(self, messages: List[Dict], turn_idx) -> Tuple[Any, Any, str]:
+        # all the important stuff goes here
+        return None
